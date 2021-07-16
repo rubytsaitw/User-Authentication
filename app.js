dd7de3a8ee915e4443ec3c36c5a6aaef2e182633
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-16 17:50:58 +0800

    feat: add mongoose and user schema

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// Mongoose
+mongoose.connect('mongodb://localhost/authentication', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// Express
+app.get('/', (req, res) => {
+  res.send('login page')
+})
 
 app.listen(3000, () => {
   console.log('App is running on http"//localhost:3000')
 })
+
