e409ea174bb762dfde9e99e862095f598a01cd67
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-18 10:33:35 +0800

    Add config/mongoose and routes

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-
-const User = require('./models/user')
-
-// Mongoose
-mongoose.connect('mongodb://localhost/authentication', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// Express
-app.get('/', (req, res) => {
-  res.render('login')
-})
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http"//localhost:3000')
 })
-
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// 引用 Express 與 Express router
+const express = require('express')
+const router = express.Router()
+
+// 引入路由模組
+const login = require('./modules/login')
+router.use('/', login)
+
+// 匯出路由器
+module.exports = router
